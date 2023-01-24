8bcb04ac83e4ec3036065e9a84e76bf06b59aec1
Your Name <you@example.com>
2023-01-24 12:29:10 -0500

    sp

--- experiment/timely/main.cpp
+++ experiment/timely/main.cpp
@@ -1,4 +1,19 @@
 #include <timely.h>
+#include <sched.h>
+
+// rdtsc pointless unless pinned
+int pinToHWCore(int coreId) {
+  assert(coreId>=0);
+
+  cpu_set_t mask;
+  CPU_ZERO(&mask);
+  CPU_SET(coreId, &mask);
+
+  if (sched_setaffinity(0, sizeof(cpu_set_t), &mask) == -1) {
+      return errno;
+  }
+
+  return 0;
 
 int main(int argc, char **argv) {
   unsigned iters = 10;
